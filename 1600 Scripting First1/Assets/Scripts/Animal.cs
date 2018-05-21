6e20e6d9e4147b41539d028dd318aa302aa32d6c
Unknown <10820789@my.uvu.edu>
2018-05-21 10:13:16 -0600

    Moving stuff

---
+++ 1600 Scripting First1/Assets/Scripts/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+    // Use this for initialization
+    public float Speed;
+    public int PowerLevel;
+    public string PlayerName;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, Speed, 0);
+        transform.Translate(Speed, 0, 0);
+		
+	}
+}
