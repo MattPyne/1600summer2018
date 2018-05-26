96fadd6c1f4d416ba71ce6f7fabd7b48ec65d58c
Unknown <10820789@my.uvu.edu>
2018-05-25 20:05:20 -0600

    Horse now runs in a circle around the path

---
+++ 1600 Scripting First1/Assets/Scripts/Scripts/RunningWild.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningWild : MonoBehaviour {
+    public float Speed;
+    public int PowerLevel;
+    public string PlayerName;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, Speed, 0);
+        transform.Translate(Speed, 0, 0);
+	}
+}
